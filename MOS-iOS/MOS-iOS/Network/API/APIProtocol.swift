b5feb90048e20e9a11bdfb74a57459164977a9f8
Woopa01 <whdnwls49@naver.com>
2019-04-25 09:17:38 +0900

    [ADD] APIProtocol, Connector

---
+++ MOS-iOS/MOS-iOS/Network/API/APIProtocol.swift
@@ -0,0 +1,14 @@
+//
+//  APIProtocol.swift
+//  MOS-iOS
+//
+//  Created by 조우진 on 22/04/2019.
+//  Copyright © 2019 조우진. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+protocol APIProtocol {
+    func getColor() -> UIColor
+}
