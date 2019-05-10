a4e99c90aa799276436fcd1cbf3df46f2611c5fe
Woopa01 <whdnwls49@naver.com>
2019-05-10 16:44:30 +0900

    [DELETE] API

--- MOS-iOS/MOS-iOS/Network/API/API.swift
+++ MOS-iOS/MOS-iOS/Network/API/API.swift
@@ -7,7 +7,25 @@
 //
 
 import Foundation
+import Alamofire
 import RxSwift
 
-protocol SignUpApi {
+protocol SignUp {
+    func SignUpRequest(params: Parameters) -> Observable<StatusCode>
+}
+
+protocol APIProvider: SignUp { }
+
+class Api: APIProvider {
+    private let connector = Connector()
+
+    func SignUpRequest(params: Parameters) -> Observable<StatusCode> {
+        return connector.get(path: MyPageApi.signup.getPath(),
+                             params: params,
+                             header: Header.Empty)
+            .map{ res, _ -> StatusCode in
+                if res.statusCode == 200 { return StatusCode.success }
+                else { return StatusCode.failure }
+        }
+    }
 }
