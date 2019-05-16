d79cbea20719fb11b43485191b2e09956da525bb
Woopa01 <whdnwls49@naver.com>
2019-05-16 15:25:03 +0900

    [ADD] LoginNodes

---
+++ MOS-iOS/MOS-iOS/Views/LoginNodes.swift
@@ -0,0 +1,67 @@
+//
+//  LoginNodes.swift
+//  MOS-iOS
+//
+//  Created by 조우진 on 16/05/2019.
+//  Copyright © 2019 조우진. All rights reserved.
+//
+
+import Foundation
+import AsyncDisplayKit
+
+class IdInputNode: ASDisplayNode {
+    
+    var idField: UITextField? {
+        return self.view as? UITextField
+    }
+    
+    override init() {
+        super.init()
+        self.setViewBlock { () -> UIView in
+            let field = UITextField()
+            field.attributedPlaceholder = NSAttributedString(string: "아이디를 입력해주세요.",attributes:
+                [.font: UIFont.systemFont(ofSize: 15),
+                 .foregroundColor: UIColor.gray])
+            return field
+        }
+        self.style.preferredSize = CGSize(width: 280.0, height: 15.0)
+    }
+}
+
+class PasswordInputNode: ASDisplayNode {
+    
+    var passwordField: UITextField? {
+        return self.view as? UITextField
+    }
+    
+    override init() {
+        super.init()
+        self.setViewBlock { () -> UIView in
+            let field = UITextField()
+            field.attributedPlaceholder = NSAttributedString(string: "비밀번호를 입력해주세요.",attributes:
+                [.font: UIFont.systemFont(ofSize: 15),
+                 .foregroundColor: UIColor.gray])
+            return field
+        }
+        self.style.preferredSize = CGSize(width: 280.0, height: 15.0)
+    }
+}
+
+class UserNameInputNode: ASDisplayNode {
+    
+    var userNameField: UITextField? {
+        return self.view as? UITextField
+    }
+    
+    override init() {
+        super.init()
+        self.setViewBlock { () -> UIView in
+            let field = UITextField()
+            field.attributedPlaceholder = NSAttributedString(string: "이름을 입력해주세요.",attributes:
+                [.font: UIFont.systemFont(ofSize: 15),
+                 .foregroundColor: UIColor.gray])
+            return field
+        }
+        self.style.preferredSize = CGSize(width: 280.0, height: 15.0)
+    }
+}
