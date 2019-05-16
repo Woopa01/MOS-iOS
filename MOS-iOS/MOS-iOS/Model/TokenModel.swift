c3ee984d67af2d0b113007801d08be8de677b2b8
Woopa01 <whdnwls49@naver.com>
2019-05-16 15:23:01 +0900

    [ADD] TokenModel

---
+++ MOS-iOS/MOS-iOS/Model/TokenModel.swift
@@ -0,0 +1,13 @@
+//
+//  TokenModel.swift
+//  MOS-iOS
+//
+//  Created by 조우진 on 16/05/2019.
+//  Copyright © 2019 조우진. All rights reserved.
+//
+
+import Foundation
+
+struct TokenModel : Codable{
+    let token: String
+}
